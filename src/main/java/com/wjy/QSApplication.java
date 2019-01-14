bf10fbfac858ad87925c8d16a3f970abe9745ba6
ybxxszl <1062837400@qq.com>
2019-01-14 21:24:48 +0800

    qs-springboot

--- src/main/java/com/wjy/QSApplication.java
+++ src/main/java/com/wjy/QSApplication.java
@@ -2,8 +2,20 @@ package com.wjy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.wjy.mapper.custom")
+@ComponentScan(basePackages = {"com.wjy"})
+@EnableScheduling
+@EnableAsync
+@EnableWebMvc
+@EnableSwagger2
 public class QSApplication {
 
     public static void main(String[] args) {
