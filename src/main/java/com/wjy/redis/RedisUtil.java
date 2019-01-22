f0ec672bf4043e0cd5fbd974745784747751bcaf
Wang Jun Yan <wjy@e-teng.com.cn>
2019-01-22 11:59:48 +0800

    qs-springboot

--- src/main/java/com/wjy/redis/RedisUtil.java
+++ src/main/java/com/wjy/redis/RedisUtil.java
@@ -1,62 +1,224 @@
 package com.wjy.redis;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
-
+/**
+ * Redis工具类
+ * 
+ * @author wjy
+ * @date 2019年1月22日
+ */
 @Component
 public final class RedisUtil {
 
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
 
-	public void set(String key, Object value) {
+	/*
+	 * common
+	 */
 
-		redisTemplate.opsForValue().set(key, value);
-
-	}
-	
-	public void set(String key, Object value, long timeout, TimeUnit unit) {
-
-		redisTemplate.opsForValue().set(key, value, timeout, unit);
-
-	}
-
-	public Object get(String key) {
-
-		Object value = redisTemplate.opsForValue().get(key);
-
-		return value;
-
-	}
-
-	public Long leftPush(String key, Object value) {
-
-		Long l = redisTemplate.opsForList().leftPush(key, value);
-
-		return l;
-
+	/**
+	 * 根据key判断缓存是否存在
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public boolean hasKey(String key) {
+		try {
+			Boolean b = redisTemplate.hasKey(key);
+			return b;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
-	public Object leftPush(String key) {
-
-		Object value = redisTemplate.opsForList().rightPop(key);
-
-		return value;
-
+	/**
+	 * 根据keys删除缓存
+	 * 
+	 * @param keys
+	 * @return
+	 */
+	public long delete(String... keys) {
+		try {
+			if (keys.length > 1) {
+				Long l = redisTemplate.delete(Arrays.asList(keys));
+				return l;
+			} else if (keys.length == 1) {
+				Boolean b = redisTemplate.delete(keys[0]);
+				if (b) {
+					return 1;
+				}
+			}
+			return 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
 	}
 
-	public Object leftPush(String key, long timeout, TimeUnit unit) {
-
-		Object value = redisTemplate.opsForList().rightPop(key, timeout, unit);
-
-		return value;
-
+	/**
+	 * 设置缓存失效时间
+	 * 
+	 * @param key
+	 * @param timeout
+	 * @return
+	 */
+	public boolean expire(String key, long timeout) {
+		try {
+			if (timeout > 0) {
+				Boolean b = redisTemplate.expire(key, timeout, TimeUnit.SECONDS);
+				return b;
+			}
+			return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
-	public Boolean add(String key, Object member, double score) {
+	/**
+	 * 根据key获取缓存失效时间
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public long getExpire(String key) {
+		try {
+			Long l = redisTemplate.getExpire(key, TimeUnit.SECONDS);
+			return l;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	/*
+	 * string
+	 */
+
+	/**
+	 * 放入缓存
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public boolean setValue(String key, Object value) {
+		try {
+			redisTemplate.opsForValue().set(key, value);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 放入缓存并且设置失效时间
+	 * 
+	 * @param key
+	 * @param value
+	 * @param timeout
+	 * @return
+	 */
+	public boolean setValue(String key, Object value, long timeout) {
+		try {
+			if (timeout > 0) {
+				redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.SECONDS);
+				return true;
+			}
+			return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 根据key获取缓存
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public Object getValue(String key) {
+		try {
+			Object value = redisTemplate.opsForValue().get(key);
+			return value;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * 根据key增加或减少缓存的值
+	 * 
+	 * @param key
+	 * @param delta
+	 * @return
+	 */
+	public long changeValue(String key, long delta) {
+		try {
+			Long l;
+			if (delta > 0) {
+				l = redisTemplate.opsForValue().increment(key, delta);
+			} else if (delta < 0) {
+				l = redisTemplate.opsForValue().decrement(key, delta);
+			} else {
+				l = (Long) redisTemplate.opsForValue().get(key);
+			}
+			return l;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	/*
+	 * list
+	 */
+
+	/**
+	 * 推送缓存
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public long leftPushList(String key, Object value) {
+		try {
+			Long l = redisTemplate.opsForList().leftPush(key, value);
+			return l;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+
+	/**
+	 * 弹出缓存
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public Object rightPopList(String key) {
+		try {
+			Object value = redisTemplate.opsForList().rightPop(key);
+			return value;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public Boolean addZSet(String key, Object member, double score) {
 
 		Boolean b = redisTemplate.opsForZSet().add(key, member, score);
 
@@ -64,7 +226,7 @@ public final class RedisUtil {
 
 	}
 
-	public Double score(String key, Object member) {
+	public Double scoreZSet(String key, Object member) {
 
 		Double score = redisTemplate.opsForZSet().score(key, member);
 
@@ -72,7 +234,7 @@ public final class RedisUtil {
 
 	}
 
-	public Long remove(String key, Object member) {
+	public Long removeZSet(String key, Object member) {
 
 		Long l = redisTemplate.opsForZSet().remove(key, member);
 
---
+++ src/test/java/com/wjy/redis/RedisTest.java
@@ -0,0 +1,46 @@
+package com.wjy.redis;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+
+	@Autowired
+	private RedisUtil redisUtil;
+
+	@Test
+	public void rightPopListTest() {
+		try {
+			while (true) {
+				Object value = redisUtil.rightPopList("list");
+				if (value == null) {
+					TimeUnit.SECONDS.sleep(10);
+					System.out.println("暂无");
+				} else {
+					System.out.println("value: " + value);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void leftPushListTest() {
+		try {
+			for (int i = 1; i <= 10; i++) {
+				redisUtil.leftPushList("list", i);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
