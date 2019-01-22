0e05449b1d2e442216c0358cdc95de757fc7b9de
Wang Jun Yan <wjy@e-teng.com.cn>
2019-01-22 15:39:48 +0800

    qs-springboot

--- src/main/java/com/wjy/swagger/SwaggerConfig.java
+++ src/main/java/com/wjy/swagger/SwaggerConfig.java
@@ -1,14 +1,22 @@
 package com.wjy.swagger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.swagger.annotations.Api;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.ApiSelectorBuilder;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -16,45 +24,86 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class SwaggerConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 
-    }
+	}
 
-    @Bean
-    public Docket createRestApi() {
+	@Bean
+	public Docket createRestApi() {
 
-        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+		Docket docket = new Docket(DocumentationType.SWAGGER_2);
 
-        docket.apiInfo(getApiInfo());
+		docket.apiInfo(getApiInfo());
+		docket.globalOperationParameters(getParameterBuilder());
 
-        return getApiSelectorBuilder(docket.select()).build();
+		return getApiSelectorBuilder(docket.select()).build();
 
-    }
+	}
 
-    private ApiSelectorBuilder getApiSelectorBuilder(ApiSelectorBuilder apiSelectorBuilder) {
+	/**
+	 * 需扫描包
+	 * 
+	 * @param apiSelectorBuilder
+	 * @return
+	 */
+	private ApiSelectorBuilder getApiSelectorBuilder(ApiSelectorBuilder apiSelectorBuilder) {
 
-        apiSelectorBuilder.apis(RequestHandlerSelectors.basePackage("com.wjy.controller"));
-        apiSelectorBuilder.paths(PathSelectors.any());
+		// 扫描包路径
+		// apiSelectorBuilder.apis(RequestHandlerSelectors.basePackage("com.wjy.controller"));
+		// 扫描类注解
+		apiSelectorBuilder.apis(RequestHandlerSelectors.withClassAnnotation(Api.class));
+		apiSelectorBuilder.paths(PathSelectors.any());
 
-        return apiSelectorBuilder;
+		return apiSelectorBuilder;
 
-    }
+	}
 
-    private ApiInfo getApiInfo() {
+	/**
+	 * 请求头配置
+	 * 
+	 * @return
+	 */
+	private List<Parameter> getParameterBuilder() {
 
-        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+		List<Parameter> parameters = new ArrayList<Parameter>();
 
-        apiInfoBuilder.title("Swagger2接口文档");
-        apiInfoBuilder.description("Swagger2接口描述");
-        apiInfoBuilder.version("1.0.0");
-        apiInfoBuilder.contact(new Contact("ybxxszl", "https://www.baidu.com/", "1062837400@qq.com"));
+		ModelRef modelRef = new ModelRef("string");
 
-        return apiInfoBuilder.build();
+		ParameterBuilder parameterBuilder1 = new ParameterBuilder();
+		ParameterBuilder parameterBuilder2 = new ParameterBuilder();
 
-    }
+		Parameter parameter1 = parameterBuilder1.name("H-AuthorId").description("作者ID").parameterType("header")
+				.defaultValue("H-AuthorId").modelRef(modelRef).required(true).build();
+		Parameter parameter2 = parameterBuilder2.name("H-Token").description("Token").parameterType("header")
+				.defaultValue("H-Token").modelRef(modelRef).required(true).build();
+
+		parameters.add(parameter1);
+		parameters.add(parameter2);
+
+		return parameters;
+
+	}
+
+	/**
+	 * 接口文档注释
+	 * 
+	 * @return
+	 */
+	private ApiInfo getApiInfo() {
+
+		ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+		apiInfoBuilder.title("Swagger2接口文档");
+		apiInfoBuilder.description("Swagger2接口描述");
+		apiInfoBuilder.version("1.0.0");
+		apiInfoBuilder.contact(new Contact("ybxxszl", "https://www.baidu.com/", "1062837400@qq.com"));
+
+		return apiInfoBuilder.build();
+
+	}
 
 }
