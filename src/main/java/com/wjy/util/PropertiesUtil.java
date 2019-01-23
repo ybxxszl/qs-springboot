b6359f257baf5ae8046c706a4b49deda0e0a1ef6
Wang Jun Yan <wjy@e-teng.com.cn>
2019-01-23 17:38:45 +0800

    properties添加读取方法

--- src/main/java/com/wjy/util/PropertiesUtil.java
+++ src/main/java/com/wjy/util/PropertiesUtil.java
@@ -9,37 +9,64 @@ import java.util.ResourceBundle;
  */
 public class PropertiesUtil {
 
-    private final static ResourceBundle resource = ResourceBundle.getBundle("resource");
+	private final static ResourceBundle resource = ResourceBundle.getBundle("resource");
 
-    /**
-     * @param key 键
-     * @return String 值
-     * @date 2018年9月9日
-     * @author ybxxszl
-     * @description 根据键获取值
-     */
-    public static String getValue(String key) {
+	/**
+	 * @param key
+	 *            键
+	 * @return String 值
+	 * @date 2018年9月9日
+	 * @author ybxxszl
+	 * @description 根据键获取值
+	 */
+	public static String getValue(String key) {
 
-        return resource.getString(key);
+		return resource.getString(key);
 
-    }
+	}
 
-    /**
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return String 值
-     * @date 2018年9月9日
-     * @author ybxxszl
-     * @description 根据键获取值，如果没有相应键，则返回默认值
-     */
-    public static String getValueOrDefault(String key, String defaultValue) {
+	/**
+	 * @param key
+	 *            键
+	 * @param defaultValue
+	 *            默认值
+	 * @return String 值
+	 * @date 2018年9月9日
+	 * @author ybxxszl
+	 * @description 根据键获取值，如果没有相应键，则返回默认值
+	 */
+	public static String getValueOrDefault(String key, String defaultValue) {
 
-        if (resource.containsKey(key)) {
-            return resource.getString(key);
-        } else {
-            return defaultValue;
-        }
+		if (resource.containsKey(key)) {
+			return resource.getString(key);
+		} else {
+			return defaultValue;
+		}
 
-    }
+	}
+
+	public static Integer getIntegerValue(String key) {
+
+		return Integer.valueOf(resource.getString(key));
+
+	}
+
+	public static Long getLongValue(String key) {
+
+		return Long.valueOf(resource.getString(key));
+
+	}
+
+	public static String getStringValue(String key) {
+
+		return resource.getString(key);
+
+	}
+
+	public static Boolean getBooleanValue(String key) {
+
+		return Boolean.valueOf(resource.getString(key));
+
+	}
 
 }
