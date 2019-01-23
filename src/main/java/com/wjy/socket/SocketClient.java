6cb38320332a1f55900fa794ffa63f39c1893f7d
Wang Jun Yan <wjy@e-teng.com.cn>
2019-01-23 17:55:00 +0800

    socket整理

--- src/main/java/com/wjy/socket/SocketClient.java
+++ src/main/java/com/wjy/socket/SocketClient.java
@@ -3,20 +3,31 @@ package com.wjy.socket;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import com.wjy.util.PropertiesUtil;
+
 public class SocketClient {
 
+	private static final String HOST = PropertiesUtil.getStringValue("socket.server.host");
+
+	private static final Integer PORT = PropertiesUtil.getIntegerValue("socket.server.port");
+
+	private static final String CODING = PropertiesUtil.getStringValue("socket.coding");
+
+	private static final String MESSAGE = "迟来的请求";
+
 	public static void main(String args[]) throws Exception {
-		// 要连接的服务端IP地址和端口
-		String host = "127.0.0.1";
-		int port = 8888;
-		// 与服务端建立连接
-		Socket socket = new Socket(host, port);
-		// 建立连接后获得输出流
-		OutputStream outputStream = socket.getOutputStream();
-		String message = "你好  yiwangzhibujian";
-		socket.getOutputStream().write(message.getBytes("UTF-8"));
-		outputStream.close();
+
+		Socket socket = new Socket(HOST, PORT);
+
+		OutputStream os = socket.getOutputStream();
+
+		os.write(MESSAGE.getBytes(CODING));
+
+		System.out.println("send message : " + MESSAGE);
+
+		os.close();
 		socket.close();
+
 	}
 
 }
--- src/main/java/com/wjy/socket/SocketServer.java
+++ src/main/java/com/wjy/socket/SocketServer.java
@@ -4,28 +4,40 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import com.wjy.util.PropertiesUtil;
+
 public class SocketServer {
 
+	private static final Integer PORT = PropertiesUtil.getIntegerValue("socket.server.port");
+
+	private static final String CODING = PropertiesUtil.getStringValue("socket.coding");
+
 	public static void main(String[] args) throws Exception {
-		// 监听指定的端口
-		int port = 8888;
-		ServerSocket server = new ServerSocket(port);
-		// server将一直等待连接的到来
-		System.out.println("server将一直等待连接的到来");
+
+		ServerSocket server = new ServerSocket(PORT);
+
+		System.out.println("ServerSocket等待中。。。");
+
 		Socket socket = server.accept();
-		// 建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
-		InputStream inputStream = socket.getInputStream();
-		byte[] bytes = new byte[1024];
+
 		int len;
+		byte[] b = new byte[1024];
 		StringBuilder sb = new StringBuilder();
-		while ((len = inputStream.read(bytes)) != -1) {
-			// 注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
-			sb.append(new String(bytes, 0, len, "UTF-8"));
+
+		InputStream is = socket.getInputStream();
+
+		while ((len = is.read(b)) != -1) {
+
+			sb.append(new String(b, 0, len, CODING));
+
 		}
-		System.out.println("get message from client: " + sb);
-		inputStream.close();
+
+		System.out.println("receive message : " + sb);
+
+		is.close();
 		socket.close();
 		server.close();
+
 	}
 
 }
