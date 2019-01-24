5480a1c8bb88a7d88f9d353f35611524b83fa769
Wang Jun Yan <wjy@e-teng.com.cn>
2019-01-24 16:23:18 +0800

    聊天室初始化

---
+++ src/main/java/com/wjy/socket/SocketServer.java
@@ -0,0 +1,51 @@
+package com.wjy.socket;
+
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.wjy.util.PropertiesUtil;
+
+public class SocketServer {
+
+	private static final Integer PORT = PropertiesUtil.getIntegerValue("socket.server.port");
+
+	private static final String CODING = PropertiesUtil.getStringValue("socket.coding");
+
+	public static void main(String[] args) throws Exception {
+
+		ServerSocket server = new ServerSocket(PORT);
+
+		System.out.println("聊天室等待中");
+
+		Socket socket = server.accept();
+
+		InputStream is = socket.getInputStream();
+
+		byte[] b;
+
+		while (true) {
+
+			int first = is.read();
+
+			if (first == -1) {
+				break;
+			}
+
+			int second = is.read();
+
+			b = new byte[(first << 8) + second];
+
+			is.read(b);
+
+			System.out.println("server receive message : " + new String(b, CODING));
+
+		}
+
+		is.close();
+		socket.close();
+		server.close();
+
+	}
+
+}
