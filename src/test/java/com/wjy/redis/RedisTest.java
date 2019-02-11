711418b4f3dd441b672adc41b85972d713042386
Wang Jun Yan <wjy@e-teng.com.cn>
2019-02-11 16:54:14 +0800

    去除由于未使用对象导致的警告

--- src/test/java/com/wjy/redis/RedisTest.java
+++ src/test/java/com/wjy/redis/RedisTest.java
@@ -2,7 +2,6 @@ package com.wjy.redis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -10,35 +9,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RedisTest {
 
-    @Autowired
-    private RedisUtil redisUtil;
+	@Test
+	public void rightPopListTest() {
+		try {
+			// while (true) {
+			// Object value = redisUtil.rightPopList("list");
+			// if (value == null) {
+			// TimeUnit.SECONDS.sleep(10);
+			// System.out.println("暂无");
+			// } else {
+			// System.out.println("value: " + value);
+			// }
+			// }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Test
-    public void rightPopListTest() {
-        try {
-//            while (true) {
-//                Object value = redisUtil.rightPopList("list");
-//                if (value == null) {
-//                    TimeUnit.SECONDS.sleep(10);
-//                    System.out.println("暂无");
-//                } else {
-//                    System.out.println("value: " + value);
-//                }
-//            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void leftPushListTest() {
-        try {
-//            for (int i = 1; i <= 10; i++) {
-//                redisUtil.leftPushList("list", i);
-//            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	@Test
+	public void leftPushListTest() {
+		try {
+			// for (int i = 1; i <= 10; i++) {
+			// redisUtil.leftPushList("list", i);
+			// }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
